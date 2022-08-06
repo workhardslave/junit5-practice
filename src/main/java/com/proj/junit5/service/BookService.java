2a6db3fe03e439ffeddedb6fdf6988721b1b02ea
workhardslave <mae0428@naver.com>
2022-08-07 02:20:22 +0900

    서비스 레이어 : 등록, 조회 기능 추가

--- src/main/java/com/proj/junit5/service/BookService.java
+++ src/main/java/com/proj/junit5/service/BookService.java
@@ -1,7 +1,46 @@
 package com.proj.junit5.service;
 
+import com.proj.junit5.domain.Book;
+import com.proj.junit5.domain.BookRepository;
+import com.proj.junit5.web.dto.BookResponseDto;
+import com.proj.junit5.web.dto.BookSaveRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Transactional(rollbackFor = RuntimeException.class)
+@RequiredArgsConstructor
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    // 등록
+    public BookResponseDto saveBook(BookSaveRequestDto dto) {
+        Book savedBook = bookRepository.save(dto.toEntity()); // 영속화된 객체는 서비스 레이어에서 빠져나가지 않도록 해야한다.
+        return new BookResponseDto().toDto(savedBook);
+    }
+
+    // 목록
+    @Transactional(readOnly = true)
+    public List<BookResponseDto> findAllBooks() {
+        return bookRepository.findAll().stream() // 1. 스트림 생성
+                .map(book -> new BookResponseDto().toDto(book)) // 2. Book의 인스턴스를 복사하여 BookResponseDto로 변환
+                .collect(Collectors.toList()); // 3. 변환된 BookResponseDto를 리스트 형태로 변환하여 리턴
+    }
+
+    // 단건
+    @Transactional(readOnly = true)
+    public BookResponseDto findBookById(Long id) {
+        Book foundBook = bookRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 id 입니다. " + id));
+        return new BookResponseDto().toDto(foundBook);
+    }
+
+    // 삭제
+
+    // 수정정
 }
--- src/main/java/com/proj/junit5/web/dto/BookResponseDto.java
+++ src/main/java/com/proj/junit5/web/dto/BookResponseDto.java
@@ -1,4 +1,19 @@
 package com.proj.junit5.web.dto;
 
+import com.proj.junit5.domain.Book;
+import lombok.Getter;
+
+@Getter
 public class BookResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookResponseDto toDto(Book book) {
+        this.id = book.getId();
+        this.title = book.getTitle();
+        this.author = book.getAuthor();
+        return this;
+    }
 }
